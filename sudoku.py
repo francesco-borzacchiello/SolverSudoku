7e2673bcee9a0d8217107be0e6e140ce0e544a3a
Francesco Borzacchiello <fran.borzacchiello@studenti.unina.it>
2021-08-02 22:15:23 +0200

    Refactoring: added the IndexOfCell class, it represents the coordinates to identify a cell in sudoku

--- sudoku.py
+++ sudoku.py
@@ -1,7 +1,14 @@
 from math import *
 from numpy import *
+from operator import *
 
 from puzzle import *
+
+class IndicesOfCell(tuple):
+    def __new__(self, row : int, column : int):
+        IndicesOfCell.row = property(itemgetter(0))
+        IndicesOfCell.column = property(itemgetter(1))
+        return tuple.__new__(IndicesOfCell, (row, column))
 
 class ClassicSudoku(Puzzle):
     
@@ -9,13 +16,11 @@ class ClassicSudoku(Puzzle):
         self.__check_input(sudoku)
         self.__make_grid(sudoku)
         self.__calculate_candidates()
-        #print(self)
     
     def __str__(self):
         return (self.__make_top_frame()
                 + self.__make_board()
                 + self.__make_bottom_frame()) 
-                # + "\n\n" + str(self.candidates)
         
     def __eq__(self, sudoku):
         return (self.__sudoku is not None
@@ -31,8 +36,7 @@ class ClassicSudoku(Puzzle):
         return (start 
                 + ((border_block + divider) * (self.__blocks_for_side_of_a_sudoku - 1)) 
                 + border_block
-                + end
-                )
+                + end)
                     
     def __make_frame_parts(self, start: str, edge: str, divider : str, end : str) -> str:
         return self.__make_frame_parts_with_additional_divider(start, edge, divider, edge, end)
@@ -58,18 +62,19 @@ class ClassicSudoku(Puzzle):
                 for column in range(len(self.__sudoku)):
                     for column_of_value in range(self.__values_for_side_of_a_block):
                         index_candidate = 0
-                        if not self.cell_is_empty(row, column):
-                            board += self.__make_cell_full(row_of_value, column_of_value, self.__sudoku[row, column])
+                        if not self.cell_is_empty(IndicesOfCell(row, column)):
+                            board += self.__make_cell_full(IndicesOfCell(row_of_value, column_of_value), 
+                                                            self.__sudoku[row, column])
                         else:
                             expected_candidate = (row_of_value * self.__values_for_side_of_a_block) + (column_of_value + 1)
-                            while (index_candidate < len(self.candidates[(row, column)])
-                                    and self.candidates[(row, column)][index_candidate] < expected_candidate):
+                            while (index_candidate < len(self.candidates[IndicesOfCell(row, column)])
+                                    and self.candidates[IndicesOfCell(row, column)][index_candidate] < expected_candidate):
                                     index_candidate += 1
-                            if  (index_candidate >= len(self.candidates[(row, column)])
-                                or self.candidates[(row, column)][index_candidate] > expected_candidate):  
+                            if  (index_candidate >= len(self.candidates[IndicesOfCell(row, column)])
+                                or self.candidates[IndicesOfCell(row, column)][index_candidate] > expected_candidate):  
                                     board += " "
                             else:
-                                board += str(self.candidates[(row, column)][index_candidate])
+                                board += str(self.candidates[IndicesOfCell(row, column)][index_candidate])
                         board += self.__make_vertical_divider_frame(column, column_of_value)
                 board += " ║\n"
             if row + 1 != (self.__blocks_for_side_of_a_sudoku * self.__values_for_side_of_a_block):
@@ -79,11 +84,11 @@ class ClassicSudoku(Puzzle):
                     board += self.__make_orizontal_divider_block()
         return board
     
-    def __make_cell_full(self, row : int, column : int, value : int) -> str:
-        if row == (column % self.__blocks_for_side_of_a_sudoku) and int(self.__blocks_for_side_of_a_sudoku / 2) == row:
-            return str(value)
-        else:
-            return "•"
+    def __make_cell_full(self, cell : IndicesOfCell, value : int) -> str:
+        if (cell.row == (cell.column % self.__blocks_for_side_of_a_sudoku) 
+            and int(self.__blocks_for_side_of_a_sudoku / 2) == cell.row):
+                return str(value)
+        return "•"
 
     def __make_vertical_divider_frame(self, column : int, column_of_cell : int) -> str:
         if (column_of_cell == self.__values_for_side_of_a_block - 1 
@@ -139,28 +144,28 @@ class ClassicSudoku(Puzzle):
             for column in range(len(self.__sudoku)):
                 candidates_for_a_cell = []
                 for candidate in range(1, len(self.__sudoku) + 1):
-                    if self.__candidate_is_eligible(row, column, candidate):
+                    if self.__candidate_is_eligible(IndicesOfCell(row, column), candidate):
                         candidates_for_a_cell.append(candidate)
                 if len(candidates_for_a_cell) > 0:
-                    self.candidates[(row, column)] = candidates_for_a_cell
+                    self.candidates[IndicesOfCell(row, column)] = candidates_for_a_cell
                 
-    def __candidate_is_eligible(self, row : int, column : int, value : int) -> bool:
-        return (self.cell_is_empty(row, column)
-                and self.__value_not_in_block(row, column, value) 
-                and self.__value_not_in_row(row, value) 
-                and self.__value_not_in_column(column, value))
+    def __candidate_is_eligible(self, cell : IndicesOfCell, value : int) -> bool:
+        return (self.cell_is_empty(cell)
+                and self.__value_not_in_block(cell, value) 
+                and self.__value_not_in_row(cell.row, value) 
+                and self.__value_not_in_column(cell.column, value))
         
-    def cell_is_empty(self, row : int, column : int) -> bool:    
-        return self.__sudoku[row, column] == 0
+    def cell_is_empty(self, cell : IndicesOfCell) -> bool:    
+        return self.__sudoku[cell.row, cell.column] == 0
 
-    def cell_has_candidate(self, row : int, column : int, candidate: int) -> bool:
-        return (row, column) in self.candidates and candidate in self.candidates[(row, column)]
+    def cell_has_candidate(self, cell : IndicesOfCell, candidate: int) -> bool:
+        return cell in self.candidates and candidate in self.candidates[cell]
         
-    def __value_not_in_block(self, row : int, column : int, candidate : int) -> bool: 
-        return candidate not in self.__sudoku[self.__first_row_of_the_block(row) : 
-                                            self.__first_row_of_the_block(row) + self.__values_for_side_of_a_block,
-                                            self.__first_column_of_the_block(column) : 
-                                            self.__first_column_of_the_block(column) + self.__values_for_side_of_a_block]
+    def __value_not_in_block(self, cell : IndicesOfCell, candidate : int) -> bool: 
+        return candidate not in self.__sudoku[self.__first_row_of_the_block(cell.row) : 
+                                            self.__first_row_of_the_block(cell.row) + self.__values_for_side_of_a_block,
+                                            self.__first_column_of_the_block(cell.column) : 
+                                            self.__first_column_of_the_block(cell.column) + self.__values_for_side_of_a_block]
     
     def __value_not_in_row(self, row : int, candidate : int) ->bool:
         return candidate not in self.__sudoku[row, : ]
@@ -172,26 +177,26 @@ class ClassicSudoku(Puzzle):
     def is_solved(self) -> bool:
         return len(self.candidates) == 0
 
-    def cell_has_only_one_candidate(self, row : int, column : int) -> bool:
-        return (self.cell_is_empty(row, column) 
-                and (row, column) in self.candidates 
-                and len(self.candidates[(row, column)]) == 1)
+    def cell_has_only_one_candidate(self, cell : IndicesOfCell) -> bool:
+        return (self.cell_is_empty(cell) 
+                and cell in self.candidates 
+                and len(self.candidates[cell]) == 1)
 
     # returns False if the operation fails, otherwise True
-    def confirm_candidate(self, row : int, column : int):
-        self.insert_value_in_cell(row, column, self.candidates[(row, column)][0])
+    def confirm_candidate(self, cell : IndicesOfCell):
+        self.insert_value_in_cell(cell, self.candidates[cell][0])
     
-    def insert_value_in_cell(self, row : int, column : int, value : int) -> bool:
-        if self.__cell_and_value_is_valid(row, column, value):
-            self.__complete_inserting_value_in_cell(row, column, value)
+    def insert_value_in_cell(self, cell : IndicesOfCell, value : int) -> bool:
+        if self.__cell_and_value_is_valid(cell, value):
+            self.__complete_inserting_value_in_cell(cell, value)
             return True
         return False
 
-    def __cell_and_value_is_valid(self, row : int, column : int, value : int) -> bool:
-        return self.__cell_is_valid(row, column) and self.__value_is_valid(value)
+    def __cell_and_value_is_valid(self, cell : IndicesOfCell, value : int) -> bool:
+        return self.__cell_is_valid(cell) and self.__value_is_valid(value)
 
-    def __cell_is_valid(self, row : int, column : int) -> bool:
-        return self.__index_is_valid(row) and self.__index_is_valid(column)
+    def __cell_is_valid(self, cell : IndicesOfCell) -> bool:
+        return self.__index_is_valid(cell.row) and self.__index_is_valid(cell.column)
     
     def __index_is_valid(self, index : int) -> bool:
         return (isinstance(index, int) 
@@ -203,30 +208,30 @@ class ClassicSudoku(Puzzle):
                 and value > 0 
                 and value <= self.values_for_side_of_a_sudoku())
 
-    def __complete_inserting_value_in_cell(self, row : int, column : int, value : int):
-            self.__sudoku[row, column] = value
-            self.__update_candidates(row, column, value)
+    def __complete_inserting_value_in_cell(self, cell : IndicesOfCell, value : int):
+            self.__sudoku[cell.row, cell.column] = value
+            self.__update_candidates(cell, value)
 
-    def __update_candidates(self, row : int, column : int, value_confirmed : int):
-        self.candidates.pop((row, column))
-        self.__udate_row_candidates(row, value_confirmed)
-        self.__udate_column_candidates(column, value_confirmed)
-        self.__update_block_candidates(self.__first_row_of_the_block(row), 
-                                        self.__first_column_of_the_block(column), value_confirmed)
+    def __update_candidates(self, cell : IndicesOfCell, value_confirmed : int):
+        self.candidates.pop(cell)
+        self.__update_row_candidates(cell.row, value_confirmed)
+        self.__update_column_candidates(cell.column, value_confirmed)
+        self.__update_block_candidates(self.__first_row_of_the_block(cell.row), 
+                                        self.__first_column_of_the_block(cell.column), value_confirmed)
 
-    def __udate_row_candidates(self, row : int, value_confirmed : int):
+    def __update_row_candidates(self, row : int, value_confirmed : int):
         for column in range(self.values_for_side_of_a_sudoku()):
-            self.__remove_a_candidate(row, column, value_confirmed)
+            self.__remove_a_candidate(IndicesOfCell(row, column), value_confirmed)
 
-    def __udate_column_candidates(self, column : int, value_confirmed : int):
+    def __update_column_candidates(self, column : int, value_confirmed : int):
         for row in range(self.values_for_side_of_a_sudoku()):
-            self.__remove_a_candidate(row, column, value_confirmed)
+            self.__remove_a_candidate(IndicesOfCell(row, column), value_confirmed)
 
     def __update_block_candidates(self,row_start : int, column_start : int, value_confirmed : int):
         for row in range(row_start, row_start + self.__values_for_side_of_a_block):
             for column in range(column_start, column_start + self.__values_for_side_of_a_block):
-                self.__remove_a_candidate(row, column, value_confirmed)
+                self.__remove_a_candidate(IndicesOfCell(row, column), value_confirmed)
 
-    def __remove_a_candidate(self, row : int, column : int, candidate_to_be_deleted : int):
-        if self.cell_has_candidate(row, column, candidate_to_be_deleted):
-            self.candidates[(row, column)].remove(candidate_to_be_deleted)
+    def __remove_a_candidate(self, cell : IndicesOfCell, candidate_to_be_deleted : int):
+        if self.cell_has_candidate(cell, candidate_to_be_deleted):
+            self.candidates[cell].remove(candidate_to_be_deleted)
--- sudokuSolver.py
+++ sudokuSolver.py
@@ -25,7 +25,7 @@ class ClassicSudokuSolver(Solver):
     def __find_cell_with_one_candidate(self):
         for row in range(self.sudoku.values_for_side_of_a_sudoku()):
             for column in range(self.sudoku.values_for_side_of_a_sudoku()):
-                self.__count_inserted += int(self.__try_to_solve_the_cell(row, column))
+                self.__count_inserted += int(self.__try_to_solve_the_cell(IndicesOfCell(row, column)))
 
     def __find_row_with_candidate_with_only_one_occurrence_and_insert_it(self):
         for row in range(self.sudoku.values_for_side_of_a_sudoku()):
@@ -34,7 +34,7 @@ class ClassicSudokuSolver(Solver):
     def __find_and_insert_candidate_with_only_one_occurence_for_this_row(self, row : int):
         for candidate in range(1, self.sudoku.values_for_side_of_a_sudoku() + 1):
             column = self.__find_the_column_in_which_to_insert_value(row, candidate)
-            self.__count_inserted += int(self.sudoku.insert_value_in_cell(row, column, candidate))
+            self.__count_inserted += int(self.sudoku.insert_value_in_cell(IndicesOfCell(row, column), candidate))
 
     def __find_the_column_in_which_to_insert_value(self, row : int, candidate : int) -> tuple:
         references_to_the_columns = []
@@ -44,7 +44,7 @@ class ClassicSudokuSolver(Solver):
 
     def __if_cell_has_this_candidate_add_it_to_the_list_of_references(self, row : int, column : int, 
                                                                         candidate : int, references : list):
-        if self.sudoku.cell_has_candidate(row, column, candidate):
+        if self.sudoku.cell_has_candidate(IndicesOfCell(row, column), candidate):
                 references.append((row, column))                                                                
 
     def __find_column_with_candidate_with_only_one_occurrence_and_insert_it(self):
@@ -54,7 +54,7 @@ class ClassicSudokuSolver(Solver):
     def __find_and_insert_candidate_with_only_one_occurence_for_this_column(self, column : int):
         for candidate in range(1, self.sudoku.values_for_side_of_a_sudoku() + 1):
             row = self.__find_the_row_in_which_to_insert_value(column, candidate)
-            self.__count_inserted += int(self.sudoku.insert_value_in_cell(row, column, candidate))
+            self.__count_inserted += int(self.sudoku.insert_value_in_cell(IndicesOfCell(row, column), candidate))
 
     def __find_the_row_in_which_to_insert_value(self, column : int, candidate : int) -> int:
         references_to_the_rows = []
@@ -64,9 +64,9 @@ class ClassicSudokuSolver(Solver):
 
 
     # returns True if it adds value in the sudoku, otherwise False 
-    def __try_to_solve_the_cell(self, row : int, column : int) -> bool:
-        if self.sudoku.cell_has_only_one_candidate(row, column):
-            self.sudoku.confirm_candidate(row, column)
+    def __try_to_solve_the_cell(self, cell : IndicesOfCell) -> bool:
+        if self.sudoku.cell_has_only_one_candidate(cell):
+            self.sudoku.confirm_candidate(cell)
             return True
         return False
         
